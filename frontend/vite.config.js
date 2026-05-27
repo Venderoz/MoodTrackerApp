1be2b48dbef94298d2fa03d75d5e5f6cfbac07d7
Ivan Soboliev <van.sob4625@gmail.com>
2026-05-27 23:30:16 +0200

    DevOps-0: Application skeleton

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/moodtracker/',
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5170', 
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  }
 })
